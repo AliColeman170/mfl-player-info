419f5caa2793eeb06ab575e6b4aca8cd2f855dbf
Ali Coleman <contact@alicoleman.com>
2023-06-03 10:08:30 +1000

    Mobile tweaks

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -8,7 +8,7 @@ export default function Player({ player }) {
     const isGoalkeeper = player.metadata.positions.includes('GK');
     
     return (
-        <div className="mx-auto w-full max-w-xl transform rounded-xl bg-white dark:bg-slate-900 shadow-2xl shadow-slate-300 dark:shadow-slate-900 ring-1 ring-black dark:ring-slate-800 ring-opacity-5 p-8">
+        <div className="mx-auto w-full max-w-xl transform rounded-xl bg-white dark:bg-slate-900 shadow-2xl shadow-slate-300 dark:shadow-slate-900 ring-1 ring-black dark:ring-slate-800 ring-opacity-5 p-6 sm:p-8">
             <div className="space-y-8 sm:space-y-0 sm:grid sm:grid-cols-3 sm:space-x-8 sm:items-center">
                 <ImageCard playerId={player.id} />
                 <BasicInfo player={player} />
