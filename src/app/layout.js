6ba55959a46ad1eb0da12f40e786861a455ed67f
Ali Coleman <contact@alicoleman.com>
2023-06-03 10:37:25 +1000

    Updated metadata and added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,13 +3,46 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'MFL Player Ratings Calculator',
   description: 'Calculate the overall rating for any Metaverse Football League (MFL) player in every position using their player ID.',
-}
+  keywords: ['MFL', '@playMFL', 'Metaverse', 'Football', 'league', 'football', 'calculator', 'player', 'ratings'],
+  authors: [{ name: 'Ali Coleman', url: 'https://www.twitter.com/alicoleman170' }],
+  applicationName: 'MFL Ratings Calculator',
+  creator: 'Ali Coleman',
+  publisher: 'Ali Coleman',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  alternates: {
+    canonical: '/'
+  },
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#f1f5f9' },
+    { media: '(prefers-color-scheme: dark)', color: '#020617' },
+  ],
+   openGraph: {
+    title: 'MFL Player Ratings Calculator',
+    description: 'Calculate the overall rating for any Metaverse Football League (MFL) player in every position using their player ID.',
+    url: 'https://mflplayer.info',
+    siteName: 'MFL Player Ratings',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'MFL Player Ratings Calculator',
+    description: 'Calculate the overall rating for any Metaverse Football League (MFL) player in every position using their player ID.',
+    site: '@alicoleman170',
+    creator: '@alicoleman170'
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -21,6 +54,7 @@ export default function RootLayout({ children }) {
           {children}
         </main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
---
+++ src/app/sitemap.js
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://mflplayer.info',
+      lastModified: new Date(),
+    },
+  ];
+}
