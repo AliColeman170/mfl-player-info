d0b27ec03a539bfb239db7340a97304b01edfdf0
Ali Coleman <ali@amigostudios.co>
2024-04-18 17:45:50 +0100

    Update deps and add loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import { ComboSearch } from "@/components/Search/ComboSearch";
-import { SinglePlayerSearch } from "@/components/Search/SinglePlayerSearch";
-import { Metadata } from "next";
+import { ComboSearch } from '@/components/Search/ComboSearch';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   alternates: {
-    canonical: "/",
+    canonical: '/',
   },
-  openGraph: { url: "https://mflplayer.info" },
+  openGraph: { url: 'https://mflplayer.info' },
 };
 
 export default function Home() {
   return (
-    <div className="flex flex-1 h-full flex-col items-center justify-start space-y-8">
-      <ComboSearch key="search" autofocus={true} />
+    <div className='flex h-full flex-1 flex-col items-center justify-start space-y-8'>
+      <ComboSearch key='search' autofocus={true} />
     </div>
   );
 }
