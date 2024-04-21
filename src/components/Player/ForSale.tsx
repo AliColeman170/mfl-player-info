e30baffdea7e06d20fd096d9acbd4650ff69f544
Ali Coleman <ali@amigostudios.co>
2024-04-21 15:34:01 +0100

    Update positioning of for sale

--- src/components/Player/ForSale.tsx
+++ src/components/Player/ForSale.tsx
@@ -20,7 +20,7 @@ export async function ForSale({ player }) {
       className='group relative rounded bg-indigo-500 px-2 py-1.5 text-[10px] font-semibold leading-none text-white ring-1 ring-inset ring-indigo-600 ring-opacity-10'
     >
       <div className='group relative flex justify-center'>
-        <span>On Sale</span>
+        <span>For Sale</span>
         <span className='absolute bottom-6 w-48 scale-0 rounded-lg bg-slate-950 p-2 text-center text-xs normal-case text-white shadow shadow-slate-300 transition-all group-hover:scale-100 dark:shadow-slate-900'>
           Available to buy for ${listingData.price}
         </span>
