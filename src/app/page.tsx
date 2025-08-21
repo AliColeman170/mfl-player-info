259d2e63f0abac43dc07e3d7759bd95ac291b810
Ali Coleman <contact@alicoleman.com>
2025-08-21 14:52:12 +0100

    Types fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,6 @@ import { RecentListingsCard } from '@/components/Dashboard/RecentListingsCard';
 import { TopRatedPlayersCard } from '@/components/Dashboard/TopRatedPlayersCard';
 import { FavoritePlayersCard } from '@/components/Dashboard/FavoritePlayersCard';
 import { TopOwnersCard } from '@/components/Dashboard/TopOwnersCard';
-import { createClient } from '@/lib/supabase/server';
-import { Card, CardContent, CardHeader } from '@/components/UI/card';
-import { getUser } from '@/data/auth';
 
 export const metadata: Metadata = {
   alternates: {
@@ -22,21 +19,6 @@ export const metadata: Metadata = {
 };
 
 export default async function Home() {
-  const supabase = await createClient();
-
-  const user = await getUser(supabase);
-
-  const { data } = await supabase
-    .from('sync_config')
-    .select('config_value')
-    .eq('config_key', 'initial_sync_in_progress')
-    .single();
-
-  if (
-    data?.config_value === '1' &&
-    user?.app_metadata.address !== '0xb6fbc6072df85634'
-  )
-    return;
   return (
     <div className='container mx-auto flex flex-col gap-y-8'>
       {/* Dashboard Grid */}
